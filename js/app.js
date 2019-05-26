d5049b51a21aac1e8b5078133bac44921d27c7b0
04M04 <micsch04@gmail.com>
2019-05-26 23:14:14 +0200

    logic is working

--- js/app.js
+++ js/app.js
@@ -1,8 +1,31 @@
-// // We listen to the resize event
-// window.addEventListener('resize', () => {
-//     // We execute the same script as before
-//     let vh = window.innerHeight * 0.01;
-//     document.documentElement.style.setProperty('--vh', `${vh}px`);
-//     let vw = window.innerWidth * 0.01;
-//     document.documentElement.style.setProperty('--vw', `${vw}px`);
-//   });
+var leftLife = 20;
+var rightLife = 20;
+var lifeElement1 = document.getElementById("lifeElement1");
+var lifeElement2 = document.getElementById("lifeElement2");
+
+function LeftLifeUp() {
+  leftLife++;
+  lifeElement1.innerHTML = leftLife;
+}
+
+function LeftLifeDown() {
+  leftLife--;
+  lifeElement1.innerHTML = leftLife;
+}
+
+function RightLifeUp() {
+  rightLife++;
+  lifeElement2.innerHTML = rightLife;
+}
+
+function RightLifeDown() {
+  rightLife--;
+  lifeElement2.innerHTML = rightLife;
+}
+
+function ResetLife() {
+  leftLife = 20;
+  rightLife = 20;
+  lifeElement1.innerHTML = leftLife;
+  lifeElement2.innerHTML = rightLife;
+}
