5679deb2e9c2f4233ce43514ea9cb4f7d7d67980
04M04 <micsch04@gmail.com>
2019-05-26 22:45:06 +0200

    design works for 1920 x 1080

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-// We listen to the resize event
-window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    let vw = window.innerWidth * 0.01;
-    document.documentElement.style.setProperty('--vw', `${vw}px`);
-  });
+// // We listen to the resize event
+// window.addEventListener('resize', () => {
+//     // We execute the same script as before
+//     let vh = window.innerHeight * 0.01;
+//     document.documentElement.style.setProperty('--vh', `${vh}px`);
+//     let vw = window.innerWidth * 0.01;
+//     document.documentElement.style.setProperty('--vw', `${vw}px`);
+//   });
